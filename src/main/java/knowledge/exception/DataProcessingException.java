5ee31fffe5d31fe140b5892a2168ff591a752374
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2022-07-06 03:50:07 +0300

    Implement custom exception & `GlobalExceptionHandler`

---
+++ src/main/java/knowledge/exception/DataProcessingException.java
@@ -0,0 +1,7 @@
+package knowledge.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
